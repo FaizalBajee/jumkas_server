eb99cc706c08c039bceda8860037ce3f4b115d2a
faizal-intech <faizal.intech@gmail.com>
2026-02-21 16:45:25 +0530

    --

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use("/auth", authRouter);
 app.use("/products", productsRouter);
 
 app.use("", (req, res) => {
-  res.json("404 Not Found");
+  res.json("404 Not Found-");
 });
 
 app.listen(8010, () => {
